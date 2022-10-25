4dbec3c09524fb6886631e54213079de36d9c8f5
Mariya Tumelya <mtumelia@gmail.com>
2022-10-25 15:59:05 +0300

    feat: add nanoid, create a state of notes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {nanoid} from "nanoid";
+
+export type NotesType = {
+  id: string
+  title: string
+  date: string
+}
 
 function App() {
-  return (
-    <div className="App">
 
-    </div>
+  const [notes, setNotes] = useState<Array<NotesType>>([
+    {
+      id: nanoid(),
+      title: "First Note",
+      date: "15/10/2022"
+    },
+    {
+      id: nanoid(),
+      title: "Second Note",
+      date: "17/10/2022"
+    },
+    {
+      id: nanoid(),
+      title: "Third Note",
+      date: "21/10/2022"
+    },
+    {
+      id: nanoid(),
+      title: "Fourth Note",
+      date: "25/10/2022"
+    }
+  ]);
+
+  return (
+      <div className="App">
+
+      </div>
   );
 }
 
